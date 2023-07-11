ae61a6be588a9c6f88c4d9d04bccc54d21036b40
Christopher Perron <cperron@c2r6p6.42quebec.com>
2023-07-10 20:01:00 -0400

    push to home

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: christo <christo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cperron <cperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 01:04:04 by christo           #+#    #+#             */
-/*   Updated: 2023/07/08 01:15:03 by christo          ###   ########.fr       */
+/*   Updated: 2023/07/10 19:53:40 by cperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include "Zombie.hpp"
 
 void	randomChump(std::string name){
-	Zombie* zombie = new Zombie(name);
-	zombie->annouce();
-	delete zombie;
+	Zombie zombie = Zombie(name);
+	zombie.annouce();
 }
